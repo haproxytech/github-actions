9a0510f750d9ea6ac1162bdc234a128c6de5a6b5
Andjelko Iharos <aiharos@haproxy.com>
2021-03-11 21:28:32 +0100

    BUG/MINOR: fix unittest and some linting errors

--- check-commit/check_test.go
+++ check-commit/check_test.go
@@ -2,17 +2,22 @@ package main
 
 import "testing"
 
-func Test_checkSubject(t *testing.T) {
+func TestCheckSubject(t *testing.T) {
+	t.Parallel()
+
+	c, _ := LoadCommitPolicy("")
+
 	type args struct {
 		subject string
 	}
+
 	tests := []struct {
 		name    string
 		args    args
 		wantErr bool
 	}{
 		{
-			name:    "bug",
+			name:    "valid type/severity",
 			args:    args{subject: "BUG/MEDIUM: config: add default location of path to the configuration file"},
 			wantErr: false,
 		},
@@ -21,10 +26,38 @@ func Test_checkSubject(t *testing.T) {
 			args:    args{subject: "BUG/MEDIUM: config: default"},
 			wantErr: true,
 		},
+		{
+			name:    "missing severity",
+			args:    args{subject: "BUG/: config: default"},
+			wantErr: true,
+		},
+		{
+			name:    "wrong tag",
+			args:    args{subject: "WRONG: config: default"},
+			wantErr: true,
+		},
+		{
+			name:    "wrong severity",
+			args:    args{subject: "BUG/WRONG: config: default"},
+			wantErr: true,
+		},
+		{
+			name:    "double spaces",
+			args:    args{subject: "BUG/MEDIUM: config:  default"},
+			wantErr: true,
+		},
+		{
+			name:    "trailing spaces",
+			args:    args{subject: "BUG/MEDIUM: config: default "},
+			wantErr: true,
+		},
 	}
+
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			if err := checkSubject(tt.args.subject); (err != nil) != tt.wantErr {
+			t.Parallel()
+			if err := c.CheckSubject([]byte(tt.args.subject)); (err != nil) != tt.wantErr {
 				t.Errorf("checkSubject() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
