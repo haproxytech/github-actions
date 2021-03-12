42b2eaa5c1299e1a792439ebd0f97fd89a2899a5
Andjelko Iharos <aiharos@haproxy.com>
2021-03-12 19:28:06 +0100

    BUG/MEDIUM: disallow unprocessed tags, apply only one definition per set

--- check-commit/check_test.go
+++ check-commit/check_test.go
@@ -17,38 +17,43 @@ func TestCheckSubject(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "valid type/severity",
+			name:    "valid type and severity",
 			args:    args{subject: "BUG/MEDIUM: config: add default location of path to the configuration file"},
 			wantErr: false,
 		},
 		{
-			name:    "bug-fail",
+			name:    "short subject",
 			args:    args{subject: "BUG/MEDIUM: config: default"},
 			wantErr: true,
 		},
 		{
 			name:    "missing severity",
-			args:    args{subject: "BUG/: config: default"},
+			args:    args{subject: "BUG/: config: default implementation"},
 			wantErr: true,
 		},
 		{
 			name:    "wrong tag",
-			args:    args{subject: "WRONG: config: default"},
+			args:    args{subject: "WRONG: config: default implementation"},
 			wantErr: true,
 		},
 		{
 			name:    "wrong severity",
-			args:    args{subject: "BUG/WRONG: config: default"},
+			args:    args{subject: "BUG/WRONG: config: default implementation"},
 			wantErr: true,
 		},
 		{
 			name:    "double spaces",
-			args:    args{subject: "BUG/MEDIUM: config:  default"},
+			args:    args{subject: "BUG/MEDIUM: config:  default implementation"},
 			wantErr: true,
 		},
 		{
 			name:    "trailing spaces",
-			args:    args{subject: "BUG/MEDIUM: config: default "},
+			args:    args{subject: "BUG/MEDIUM: config: default implementation "},
+			wantErr: true,
+		},
+		{
+			name:    "unprocessed tags remain",
+			args:    args{subject: "BUG/MINOR: MAJOR: config: default implementation"},
 			wantErr: true,
 		},
 	}
